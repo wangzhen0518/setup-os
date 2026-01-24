0da732ce3b2933bc4e0f7c4a26320677182804f8
wangzhen0518 <wangzhen0518@126.com>
2026-01-24 19:16:17 +0800

    update powershell config

--- config/shell/powersehll/Microsoft.PowerShell_profile.ps1
+++ config/shell/powersehll/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,3 @@
-<#
- * FileName: Microsoft.PowerShell_profile.ps1
- * Author: 刘 鹏
- * Email: littleNewton6@outlook.com
- * Date: 2021, Aug. 21
- * Copyright: No copyright. You can use this code for anything with no warranty.
-#>
-
-
 #------------------------------- Import Modules BEGIN -------------------------------
 # 引入 posh-git
 Import-Module posh-git
@@ -15,17 +6,9 @@ Import-Module posh-git
 Import-Module PSReadLine
 Import-Module Dircolors
 
-# 引入 oh-my-posh
 oh-my-posh init pwsh --config "${env:POSH_THEMES_PATH}/my-theme.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/negligible.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/kali.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/hunk.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/honukai.omp.json" | Invoke-Expression
 
 #------------------------------- Import Modules END   -------------------------------
-
-
-
 
 
 #-------------------------------  Set Hot-keys BEGIN  -------------------------------
@@ -52,73 +35,11 @@ Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 #-------------------------------  Set Hot-keys END    -------------------------------
 
 
-
-
-
-#-------------------------------    Functions BEGIN   -------------------------------
-# Python 直接执行
-# $env:PATHEXT += ";.py"
-
-# 更新系统组件
-# function Update-Packages {
-# 	# update pip
-# 	Write-Host "Step 1: 更新 pip" -ForegroundColor Magenta -BackgroundColor Cyan
-# 	$a = pip list --outdated
-# 	$num_package = $a.Length - 2
-# 	for ($i = 0; $i -lt $num_package; $i++) {
-# 		$tmp = ($a[2 + $i].Split(" "))[0]
-# 		pip install -U $tmp
-# 	}
-
-	# update TeX Live
-	#$CurrentYear = Get-Date -Format yyyy
-	#Write-Host "Step 2: 更新 TeX Live" $CurrentYear -ForegroundColor Magenta -BackgroundColor Cyan
-	#tlmgr update --self
-	#tlmgr update --all
-
-	# update Chocolotey
-	#Write-Host "Step 3: 更新 Chocolatey" -ForegroundColor Magenta -BackgroundColor Cyan
-	#choco outdated
-# }
-#-------------------------------    Functions END     -------------------------------
-
-
-
-
-
 #-------------------------------   Set Alias BEGIN    -------------------------------
-# 1. 编译函数 make
-# function MakeThings {
-# 	nmake.exe $args -nologo
-# }
-#Set-Alias -Name make -Value MakeThings
-#Set-Alias -Name python3 -Value python
 
-# 2. 更新系统 os-update
-# Set-Alias -Name os-update -Value Update-Packages
-
-# 3. 查看目录 ls & ll
-#function ListDirectory {
-#	(Get-ChildItem).Name
-#	Write-Host("")
-#}
-#Set-Alias -Name ls -Value ListDirectory
 Set-Alias -Name ll -Value Get-ChildItem
-#Set-Alias -Name rm -Value Remove-Item
 Set-Alias -Name touch -Value New-Item
 
-# 4. 打开当前工作目录
-# function OpenCurrentFolder {
-# 	param
-# 	(
-		# 输入要打开的路径
-		# 用法示例：open C:\
-		# 默认路径：当前工作文件夹
-# 		$Path = '.'
-# 	)
-# 	Invoke-Item $Path
-# }
-# Set-Alias -Name open -Value OpenCurrentFolder
 #-------------------------------    Set Alias END     -------------------------------
 
 
@@ -126,16 +47,8 @@ Set-Alias -Name touch -Value New-Item
 Invoke-Expression (&scoop-search --hook)
 #-------------------------------    Set Scoop END     -------------------------------
 
-#-------------------------------   Set Network BEGIN    -------------------------------
-function proxy_on {
-	$env:HTTP_PROXY="http://127.0.0.1:10809"
-	$env:HTTPS_PROXY="https://127.0.0.1:10809"
-}
-function proxy_off{
-	$env:HTTP_PROXY=""
-	$env:HTTPS_PROXY=""
-}
 
+#-------------------------------   Set Network BEGIN    -------------------------------
 function ssh-copy-id([string]$userAtMachine, $args){   
     $publicKey = "$ENV:USERPROFILE" + "/.ssh/id_rsa.pub"
     if (!(Test-Path "$publicKey")){
@@ -145,12 +58,13 @@ function ssh-copy-id([string]$userAtMachine, $args){
         & cat "$publicKey" | ssh $args $userAtMachine "umask 077; test -d .ssh || mkdir .ssh ; cat >> .ssh/authorized_keys || exit 1"      
     }
 }
-
-function my_port_forward{
-    echo $args[0]
-    ssh -L $args[0]:localhost:$args[0] -p 33111 zhenwang@202.38.69.241
-
-}
-
 #-------------------------------   Set Network END  ----------------------------------
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+
+#-------------------------------   Set Node Begin  ----------------------------------
+
+#fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
+
+#-------------------------------   Set Node End  ----------------------------------
 
